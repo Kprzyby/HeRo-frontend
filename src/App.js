d7c5bf1d750bcee9da2f780f213d6158a3cf6081
Michał Ryniewicz <misiekxyz18@gmail.com>
2022-11-30 01:39:02 +0100

    added auth service, added page routing and login, logout functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import './App.css';
 
-function App() {
+import AuthService from "./services/auth.service";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { Container } from "react-bootstrap";
+
+const App = () => {
+  const [currentUser, setCurrentUser] = useState("");
+
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user)
+    }
+
+  }, [])
+
+  const logOut = () => {
+    AuthService.logout();
+    setCurrentUser("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App min-vh-100">
+      <nav className="navbar navbar-expand navbar-dark bg-dark px-3">
+        <Link to={"/"} className="navbar-brand">
+          HeRo
+        </Link>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/home"} className="nav-link">
+              Home
+            </Link>
+          </li>
+        </div>
+        {currentUser ? (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/profile"} className="nav-link">
+                {currentUser.username}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a href="/login" className="nav-link" onClick={logOut}>
+                LogOut
+              </a>
+            </li>
+          </div>
+        ) : (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            </li>
+          </div>
+        )}
+      </nav>
+
+      <Container className="mt-3">
+        <Routes>
+          <Route exact path={"/"} element={<Home />} />
+          <Route exact path={"/home"} element={<Home />} />
+          <Route exact path={"/login"} element={<Login />} />
+        </Routes>
+      </Container>
     </div>
   );
 }
