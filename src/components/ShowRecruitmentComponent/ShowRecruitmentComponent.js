0229749b1e4956fd63fe4f848a650b07f5205350
Michał Ryniewicz <misiekxyz18@gmail.com>
2022-12-09 21:24:51 +0100

    linked candidate form with recruitment

--- src/components/ShowRecruitmentComponent/ShowRecruitmentComponent.js
+++ src/components/ShowRecruitmentComponent/ShowRecruitmentComponent.js
@@ -2,15 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ShowRecruitmentComponent.module.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Link} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-function ShowRecruitmentComponent(props){
-  const timeLeft=Math.floor((Date.parse(props.endingDate)-Date.parse(props.beginningDate)) / (1000*60*60*24));
+function ShowRecruitmentComponent(props) {
+  const timeLeft = Math.floor((Date.parse(props.endingDate) - Date.parse(props.beginningDate)) / (1000 * 60 * 60 * 24));
+  const navigate = useNavigate();
+  const moveToForm = (e) => {
+    navigate('/candidate', { state: { id: props.id } });
+  }
 
-  return(
+  return (
     <div className={styles.ShowRecruitmentComponent}>
       <div className={styles.positioningContext}>
-      <Link className='btn btn-primary' style={{position:'absolute', top:0, right:0}} to={""}>Apply</Link>
+        <button className='btn btn-primary' style={{ position: 'absolute', top: 0, right: 0 }} onClick={moveToForm}>Apply</button>
         <br></br><br></br>
         <span className={styles.title}>{props.name} &#40;{props.seniority}&#41;</span><span className={styles.expiryDate}>{timeLeft}d left</span><br></br>
         <p>Localization - {props.localization}</p>
@@ -20,11 +24,12 @@ function ShowRecruitmentComponent(props){
 }
 
 ShowRecruitmentComponent.propTypes = {
-  beginningDate:PropTypes.string,
-  endingDate:PropTypes.string,
-  name:PropTypes.string,
-  localization:PropTypes.string,
-  seniority:PropTypes.string
+  beginningDate: PropTypes.string,
+  endingDate: PropTypes.string,
+  name: PropTypes.string,
+  localization: PropTypes.string,
+  seniority: PropTypes.string,
+  id: PropTypes.number
 };
 
 ShowRecruitmentComponent.defaultProps = {};
